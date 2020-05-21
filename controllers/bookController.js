a8642819b3040b796dd0974e89175bc0ea550ec3
Edson <edson_csl@hotmail.com>
2020-05-21 02:18:59 -0300

    add method update, list for donation and list for exchange

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -18,7 +18,57 @@ module.exports = {
     } catch (error) {
       console.log(error)
     }
-   
+  },
 
+  async listBookForDonation (req, res) {
+    const books = await Livro.findAll({
+      where: {
+        disponibilidade: 'doar',
+        users_id: req.session.user.id
+      }
+    })
+    res.render('listar-meus-livros-para-doacao', {usuario:req.session.user, books})
+  },
+
+  async listBookForExchange (req, res) {
+    const books = await Livro.findAll({
+      where: {
+        disponibilidade: 'emprestar',
+        users_id: req.session.user.id
+      }
+    })
+    //books.map(item => console.log(item.titulo))
+    res.render('listar-meus-livros-para-troca', {usuario:req.session.user, books})
+  },
+
+  async updateBook (req, res) {
+    const { idBook, titulo, autor, disponibilidade, localizacao } = req.body
+
+    await Livro.update({
+      titulo,
+      autor,
+      disponibilidade,
+      localizacao,
+    }, {
+      where: { id: idBook }
+    })
+    if(disponibilidade == 'doar'){
+      const books = await Livro.findAll({
+        where: {
+          disponibilidade: 'doar',
+          users_id: req.session.user.id
+        }
+      })
+      res.render('listar-meus-livros-para-doacao', {usuario:req.session.user, books})
+    } else {
+      const books = await Livro.findAll({
+        where: {
+          disponibilidade: 'emprestar',
+          users_id: req.session.user.id
+        }
+      })
+      res.render('listar-meus-livros-para-troca', {usuario:req.session.user, books})
+      
+    }
   }
 }
