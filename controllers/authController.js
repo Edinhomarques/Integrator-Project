bb4648508c4caee17420358a7b7c448ebb3be4fb
Daniel Soares <danielandradesoares@hotmail.com>
2020-05-27 19:41:16 -0300

    Adjusting login validation feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,18 +12,18 @@ const authController = {
       }
     });
 
-    const usuarioContato = await Contato.findOne({
-      where:{
-        users_id: usuario.dataValues.id,
-      }
-    });
-
     if (!usuario || !bcrypt.compareSync(password, usuario.dataValues.password)) {
       res.render("home", {
         msgLogin: "Email ou senha errados!",
         title: 'Home'
       });
     }else {
+      const usuarioContato = await Contato.findOne({
+        where:{
+          users_id: usuario.dataValues.id,
+        }
+      });
+
       req.session.user = {
         id: usuario.dataValues.id,
         email: usuario.dataValues.email,
@@ -43,7 +43,7 @@ const authController = {
       books.map(book => console.log(book.user.first_name))
       const booksForDonation = books.filter(book => book.disponibilidade != 'emprestar') 
       const booksForSwap = books.filter(book => book.disponibilidade != 'doar') 
-      
+
       res.render('home-logado', { usuario: req.session.user, title: 'Home', booksForDonation, booksForSwap});
     }
   },
